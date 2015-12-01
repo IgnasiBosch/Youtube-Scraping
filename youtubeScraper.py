7b477eac5894f89a65de25e4d400b1cf1a6556b3
Ignasi Bosch <ibosch@afilias.info>
2015-12-01 17:09:55 +0000

    Implement mongodb + lists iterations

--- youtubeScraper.py
+++ youtubeScraper.py
@@ -2,12 +2,38 @@
 
 import time
 import sys
+import urlparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
+from humbledb import Document, Mongo, Embed
+
+
+class VideoSource(Document):
+    config_database = 'VideoScraper'
+    config_collection = 'videos'
+
+    title = 'title'
+    url = 'url'
+    ref = 'ref'
+    source = Embed('source_key')
+
+    @staticmethod
+    def is_valid(video):
+        black_list = ['[Deleted Video]', '[Private Video]']
+        return video.title not in black_list
+
+
+# class Source(Document):
+#     config_database = 'VideoScraper'
+#     config_collection = 'sources'
+#
+#     title = 'title'
+#     url = 'url'
+#     type = 'type'
 
 
 def init_driver():
@@ -17,18 +43,54 @@ def init_driver():
     return driver
 
 
-def lookup(driver, url):
-    driver.get(url + "/videos?view=0&sort=dd&flow=list&live_view=500")
+def lookup_from_list(driver, url, type):
+    driver.get(url)
     while load_more_content():
         pass
 
     html = driver.page_source
     bs = BeautifulSoup(html, "html.parser")
 
-    h3s = bs.find_all("h3", {"class": "yt-lockup-title "})
+    items = bs.find_all("a", {"class": "yt-uix-tile-link"})
 
-    for h3 in h3s:
-        print(h3.get_text() + " https://youtube.com" + h3.find("a", href=True)['href'])
+    with Mongo:
+        # source = Source.find_one({Source.url: url})
+        #
+        # if source is None:
+        #     source = Source()
+        #     source.title = bs.title.get_text().strip()
+        #     source.url = url
+        #     source.type = type
+        #     Source.insert(source)
+
+        for item in items:
+            ref = get_video_ref_from_url(item['href'])
+            video = VideoSource.find_one({VideoSource.ref: ref})
+            if video is None:
+                video = VideoSource()
+                video.title = item.get_text().strip()
+                video.url = "https://youtube.com" + item['href']
+                video.ref = ref
+                # video.source = source
+                VideoSource.insert(video)
+
+            print(video.title + " - " + video.url)
+
+
+def lookup_from_playlist(driver, url):
+    driver.get(url + "/playlists")
+    while load_more_content():
+        pass
+
+    html = driver.page_source
+    bs = BeautifulSoup(html, "html.parser")
+
+    rows = bs.select("div.yt-lockup-content a.yt-uix-sessionlink.spf-link")
+
+    for row in rows:
+        url = "https://youtube.com" + row['href']
+        lookup_from_list(driver, url, "playlist")
+        # lookup_from_list(driver, url + "/videos?view=0&sort=dd&flow=list&live_view=500", "list")
 
 
 def load_more_content():
@@ -44,10 +106,14 @@ def load_more_content():
         return False
 
 
+def get_video_ref_from_url(url):
+    return urlparse.parse_qs(urlparse.urlsplit(url).query)['v'][0]
+
 if __name__ == "__main__":
     driver = init_driver()
     url = sys.argv[1]
     try:
-        lookup(driver, url)
+       lookup_from_playlist(driver, "https://www.youtube.com/user/MIT")
+       # lookup_from_list(driver, url + "/videos?view=0&sort=dd&flow=list&live_view=500", "list")
     finally:
-        driver.quit()
+       driver.quit()
